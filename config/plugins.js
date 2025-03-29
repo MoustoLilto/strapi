71538be56470773f71dd12171455494984681c20
Moustapha Sene <moustapha.b.sene@gmail.com>
2025-03-29 23:56:44 +0100

    add of nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
     },
   },
 
+  // Cloudinary
   upload: {
     config: {
       provider: 'cloudinary',
@@ -14,11 +15,31 @@ module.exports = ({ env }) => ({
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
-      
+
       actionOptions: {
         upload: {},
         uploadStream: {},
         delete: {},
+      },
+    },
+  },
+
+  // Email
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'hello@example.com',
+        defaultReplyTo: 'hello@example.com',
       },
     },
   },
